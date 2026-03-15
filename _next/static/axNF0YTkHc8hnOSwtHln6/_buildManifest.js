02789f6b5ac76967a577b8ad45e58c06ed3fbef9
bsuarez- <beny.suarez_piedra@me.com>
2026-03-16 00:26:49 +0100

    Updates

---
+++ _next/static/axNF0YTkHc8hnOSwtHln6/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/jeremy_bocquet//_next/:path+",
+        "destination": "/jeremy_bocquet/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
