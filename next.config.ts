90c787fe53111a44df33a522427db4378118da4a
bsuarez- <beny.suarez_piedra@me.com>
2026-03-16 00:30:49 +0100

    again

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/jeremy_bocquet",
+  assetPrefix: "/jeremy_bocquet/",
   images: {
     unoptimized: true,
   },
